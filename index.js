1e44e22ee8e2f183ffe903ca012dd0566a176c0b
ryan210757 <kryanharris1@gmail.com>
2023-04-26 23:16:00 -0500

    css update

--- index.js
+++ index.js
@@ -14,55 +14,82 @@ fetch("https://fortnite-api.com/v2/shop/br")
                 sectionName.push(item.section.name)
                // sectionName.push(item.newDisplayAsset.materialInstances[0].images.OfferImage)
             }
-            
-            
         })
         
         for(let i=0;i<sectionName.length;i++){
             document.getElementById("div").innerHTML += 
-                        `
-                        <h2>${sectionName[i]}</h2>
-                
-                        `
+                `<h2>${sectionName[i]}</h2>`
+
             entryArray.filter(item => 
                 {
                     if(item.section.name === sectionName[i]){
                         console.log(item)
-                        let style
-                        switch(item.items[0].rarity.value){
-                            case "uncommon":
-                                document.getElementById("div").style.border = '3px solid #88e339'
+                        let rare = item.items[0].rarity.value
+                    switch(rare){
+                        case "uncommon":
+                            document.getElementById("div").innerHTML += 
+                            `
+                                <div id="square" class="rarity-uncommon">
+                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
+                                <p>${item.regularPrice}</p>
+                                <p>${item.items[0].name}</p>
+                                </div>
+                            `
                                 break;
-                            case "rare":
-                                style
+                        case "rare":
+                            document.getElementById("div").innerHTML += 
+                            `
+                                <div id="square" class="container rarity-rare">
+                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
+                                <p>${item.regularPrice}</p>
+                                <p>${item.items[0].name}</p>
+                                </div>
+                            `
                                 break;
-                            case "epic":
-                                style
+                        case "epic":
+                            document.getElementById("div").innerHTML += 
+                            `
+                                <div id="square" class="container rarity-epic">
+                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
+                                <p>${item.regularPrice}</p>
+                                <p>${item.items[0].name}</p>
+                                </dip
+                            `
                                 break;
-                            case "legendary":
-                                style
+                        case "legendary":
+                            document.getElementById("div").innerHTML += 
+                            `
+                                <div id="square" class="container rarity-legendary">
+                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
+                                <p>${item.regularPrice}</p>
+                                <p>${item.items[0].name}</p>
+                                </div>
+                            `
                                 break;
-                            case "icon":
-                                document.getElementById("div").style.border = '3px solid #88e339'
-                                document.getElementById("div").style.background = '3px solid #52e0e0'
+                        case "icon":
+                            document.getElementById("div").innerHTML += 
+                            `
+                                <div id="square" class="container rarity-icon_series">
+                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
+                                <p>${item.regularPrice}</p>
+                                <p>${item.items[0].name}</p>
+                                </div>
+                            `
                                 break;
-                            case "Gaming Legends Series":
-                                style
+                        case "Gaming Legends Series":
+                            document.getElementById("div").innerHTML += 
+                            `
+                                <div id="square" class="container rarity-gaming_legends">
+                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
+                                <p>${item.regularPrice}</p>
+                                <p>${item.items[0].name}</p>
+                                </div>
+                            `
                                 break;
                         }
-                        document.getElementById("div").innerHTML += 
-                        `
-                        <div class="container"">
-                        <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
-                        <p>${item.regularPrice}</p>
-                        <h1>${item.items[0].name}</h1>
-                        </div>
-                        `
                     }
                 })
         }
-
         console.log(sectionName)
-      
-    })
+      })
 
