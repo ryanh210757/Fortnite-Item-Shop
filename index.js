d35504a17f297c6d74e4b9240a32ff8965284085
ryanh210757 <kryanharris1@gmail.com>
2023-06-02 12:50:27 -0500

    code refactor

--- index.js
+++ index.js
@@ -1,115 +1,53 @@
 fetch("https://fortnite-api.com/v2/shop/br")
     .then(res => res.json())
     .then(data => {
-    
         const sectionName = []
-        const {data: {specialFeatured: {entries}}} = data //object destructuring
+        const {data: {specialFeatured: {entries}}} = data //object destructuring for special featured
         const vBuckIcon = data.data.vbuckIcon
-        const entryArray = entries.map(item => {
-            return item
-         }) 
-        console.log(entryArray)
+        const entryArray = entries.map(item => item) 
+
+        document.getElementById('mainDiv').addEventListener("click", function(e){
+            console.log(entryArray)
+        })
 
         entryArray.forEach(item => {
             if(!sectionName.includes(item.section.name)){
                 sectionName.push(item.section.name)
-               // sectionName.push(item.newDisplayAsset.materialInstances[0].images.OfferImage)
             }
         })
-        
+
+        let html
+        let rarity
+        let newArray
         for(let i=0;i<sectionName.length;i++){
-            document.getElementById("mainDiv").innerHTML += 
-                `<h2>${sectionName[i]}</h2>`
 
-            entryArray.filter(item => 
-                {
-                    if(item.section.name === sectionName[i]){
-                        console.log(item)
-                        let rarity = item.items[0].rarity.value
-                    switch(rarity){
-                        case "uncommon":
-                            document.getElementById("mainDiv").innerHTML += 
-                            `
-                                <div id="square" class="container rarity-uncommon">
-                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
-                                <p>${item.regularPrice}</p>
-                                <p>${item.items[0].name}</p>
-                                </div>
-                            `
-                                break;
-                        case "rare":
-                            document.getElementById("mainDiv").innerHTML += 
-                            `
-                                <div id="square" class="container rarity-rare">
-                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
-                                <p>${item.regularPrice}</p>
-                                <p>${item.items[0].name}</p>
-                                </div>
-                            `
-                                break;
-                        case "epic":
-                            document.getElementById("mainDiv").innerHTML += 
-                            `
-                                <div id="square" class="container rarity-epic">
-                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
-                                <p>${item.regularPrice}</p>
-                                <p>${item.items[0].name}</p>
-                                </dip
-                            `
-                                break;
-                        case "legendary":
-                            document.getElementById("mainDiv").innerHTML += 
-                            `
-                                <div id="square" class="container rarity-legendary">
-                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
-                                <p>${item.regularPrice}</p>
-                                <p>${item.items[0].name}</p>
-                                </div>
-                            `
-                                break;
-                        case "icon":
-                            document.getElementById("mainDiv").innerHTML += 
-                            `
-                                <div id="square" class="container rarity-icon_series">
-                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
-                                <p>${item.regularPrice}</p>
-                                <p>${item.items[0].name}</p>
-                                </div>
-                            `
-                                break;
-                        case "starwars":
-                            document.getElementById("mainDiv").innerHTML += 
-                            `
-                                <div id="square" class="container starwars">
-                                    <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
-                                <div class="itemInfo">
-                                    <h4>${item.items[0].name}</h4>
-                                <div class="itemBox">
-                                    <img class="vbuck" src="${vBuckIcon}"/>
-                                    <p>${item.regularPrice}</p>
-                                </div>
-                                </div>
-                                </div>
-                            `
-
-                                break;
-                        case "Gaming Legends Series":
-                            document.getElementById("mainDiv").innerHTML += 
-                            `
-                                <div id="square" class="container rarity-gaming_legends">
-                                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
-                                <p>${item.regularPrice}</p>
-                                <p>${item.items[0].name}</p>
-                                </div>
-                            `
-                            break;
-                        }
-                    }
-                })
+            newArray =  entryArray.filter(item => item.section.name === sectionName[i])
+            
+            console.log(newArray)
+            document.getElementById("mainDiv").innerHTML = `<h2>${sectionName[i]}</h2>`
+            newArray.forEach(item =>{
+                
+                html +=  `   <a>
+                <div class="container icon">
+                <img src="${item.newDisplayAsset.materialInstances[0].images.OfferImage}" />
+                <div class="itemInfo">
+                    <h4>${item.items[0].name}</h4>
+                <div class="itemBox">
+                    <img class="vbuck" src="${vBuckIcon}"/>
+                    <p>${item.regularPrice}</p>
+                </div>
+                </div>
+                </div>
+                </a>
+            `
+                document.getElementById("mainDiv").innerHTML +=  html
+            })
+            
         }
+        
         console.log(sectionName)
+   
+        
       })
         
-      document.getElementById("mainDiv").addEventListener("click", function(e){
-                console.log(e.target)
-        })
+     
